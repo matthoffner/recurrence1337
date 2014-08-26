755429d4a14409063d7b454c57c55b83f359ca3b
matthoffner <matthoffner@gmail.com>
2014-08-26 12:53:00 -0700

    Added row count

--- Master.py
+++ Master.py
@@ -1,20 +1,39 @@
 from multiprocessing import Process, Queue, Pool
 from subprocess import call
-import re, codecs, Queue
+import sys
+import re, codecs, Queue, os
+import time
 
 ScrapeQueue = Queue.Queue()
 
 def fork(toDo):
+	sys.stdout = open('log.txt', 'w')
+	start = time.time()
+	print start
+	os.chdir("/home/matth/new-coder/scrape/")
 	print "Acquired scrape: %s"%(toDo)
-	call(["python", "%s"%toDo])
+	name = "%s"%(toDo).replace(".py","")
+	call(["mkdir", "-p", name])
+	call(["cp", toDo, "-t", name])
+	os.chdir("./%s"%name)
+	print call(["python", name + ".py"])
+	cfile = re.compile("^.*csv$")
+	for fname in os.listdir("/home/matth/new-coder/scrape/%s"%(name)):
+		if cfile.match(fname):
+			csvfile = fname
 	print "Completed %s"%(toDo)
+	print "Filename written:"
+	print name
+	print "Number of rows:"
+	print open('%s'%(csvfile), 'r').read().count("\n")
+	print "Completed in:"
+	print str(time.time() - start)
 
 if __name__ == '__main__':
-	p = Pool(5)
+	p = Pool(2)
 	ScrapeQueue = []
 	i=1
-	while i < 11:
-		ScrapeQueue.append("random%d.py"%i)
-		i+=1
+	for line in codecs.open("scrapelist.txt","r","utf-8"):
+		ScrapeQueue.append(line.strip())
 	print ScrapeQueue
 	p.map(fork, ScrapeQueue)
